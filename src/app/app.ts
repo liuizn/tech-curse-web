660db7a8b0a7abc8b65b3efd89bfc149f67795c4
Luiz Nogueira <luizgabrielsnn@gmail.com>
2026-09-18 21:37:15 -0300

    feat: registrar interceptors, toaster e tema na raiz da aplicacao

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HlmToasterImports } from '@spartan-ng/helm/sonner';
+import { TemaService } from './core/tema/tema.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmToasterImports],
   templateUrl: './app.html',
 })
-export class App {}
+export class App {
+  private readonly tema = inject(TemaService);
+}
